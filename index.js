ced76a897d604144e4c3cc30e4a99390483360d1
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-08-30 16:43:17 +0000

    working

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const app =  express()
+const port = 3000 
+
+app.get('/', (req , res) =>{
+    res.send("my backend series")
+})
+
+app.listen(port , () =>{
+    console.log(`example app listening ${port}`)
+})
