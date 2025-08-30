9609d052c440bd156c42c0f991e5353ff97d2ee9
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-08-30 18:16:39 +0000

    working

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const app =  express()
 const port = 3000 
 
 app.get('/', (req , res) =>{
-    res.send("my backend series")
+    res.send('<h1>WellCome Backend Series</h1>')
+})
+app.get('/signup',(req ,res)=>{
+ res.send('<h2>Sig-nUp</h2>'
+ )
+})
+app.get('/logIn' , (req , res) => {
+    res.send('login')
 })
 
 app.listen(port , () =>{
