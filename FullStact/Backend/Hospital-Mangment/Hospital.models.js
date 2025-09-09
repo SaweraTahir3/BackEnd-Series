8b5a531f284e7815ed5958f2f7c8fe252a6aab68
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-09-09 16:28:23 +0500

    Create Hospital.models.js

---
+++ FullStact/Backend/Hospital-Mangment/Hospital.models.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+
+const hospitalSchema = new mongoose.Schema({
+
+
+
+
+},{timestamps : true})
+
+export const hospital = mongoose.model("hospital" , hospitalSchema ) 
