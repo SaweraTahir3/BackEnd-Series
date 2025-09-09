96386dcb6e272d2d878e558b39f6fef17269d2f2
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-09-09 16:56:54 +0500

    working

--- FullStact/Backend/Hospital-Mangment/Patients.models.js
+++ FullStact/Backend/Hospital-Mangment/Patients.models.js
@@ -2,7 +2,18 @@ import mongoose from 'mongoose'
 
 
 const patientSchema = new mongoose.Schema ({
+ name :{
+ type : String,
+required : true, 
+lowercase : true,
+}
 
+
+
+
+},{timestamps : true})
+
+export const medical = mongoose.model("medical" , medicalRecordSchema)
 
 
 
