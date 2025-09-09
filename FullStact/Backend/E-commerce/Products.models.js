b80fd2fc4ee255275eaf111adf1e7bf833d5afa1
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-09-09 14:36:26 +0500

    complete 💯

--- FullStact/Backend/E-commerce/Products.models.js
+++ FullStact/Backend/E-commerce/Products.models.js
@@ -1,1 +1,46 @@
-work
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.schema({
+
+name : {
+type : string,
+required: true ,
+},
+
+description: {
+type : string,
+required: true ,
+},
+
+productImage :{
+type : string,
+}, 
+
+
+price: {
+type : Number,
+default: 0 ,
+}, 
+
+
+stock :{
+ type : Number, 
+default :0 ,
+},
+
+
+ category: {
+ type: mongoose.Schema.Types.ObjectId,
+ref : "category",
+required: true,
+},
+
+owner :{
+type : mongoose.Schema.Types.ObjectId ,
+ref:"User" ,
+},
+
+}, {timestamps: true})
+
+
+export const products  mongoose.model("products" , productSchema)
