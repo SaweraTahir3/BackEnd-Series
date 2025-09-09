d9dc4aaf02de4264a0c1f98c5956e561b3047d31
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-09-09 15:16:32 +0500

    Update Order.models.js

--- FullStact/Backend/E-commerce/Order.models.js
+++ FullStact/Backend/E-commerce/Order.models.js
@@ -1,1 +1,47 @@
-work
+import mongoose from "mongoose"
+
+const orderItemSchema = new mongoose.Schema({
+
+productId : {
+type : mongoose.Schema.Types.ObjectId ,
+ref : "product",
+},
+
+quantity : {
+type: Number ,
+required : true,
+},
+})
+
+
+
+const orderSchema =  new mongoose.Schema({
+
+ order price : {
+type: Number,
+required:true,
+},
+
+customer : {
+type : mongoose.Schema.Types.ObjectId ,
+ref : "User",
+required :  true ,
+},
+
+orderItem: {
+type : [orderItemSchema]
+},
+
+address: {
+type: String,
+required: true , 
+},
+
+status: {
+type: String,
+enum : 
+
+
+} , {timestamps : true)
+
+export const order = mongoose.model("order" , orderSchema)
