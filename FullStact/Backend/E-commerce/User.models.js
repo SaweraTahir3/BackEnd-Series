22aa4ef299d8789ca0ca7fc9a7316410c51c1643
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-09-09 14:04:36 +0500

    Update User.models.js

--- FullStact/Backend/E-commerce/User.models.js
+++ FullStact/Backend/E-commerce/User.models.js
@@ -1,5 +1,24 @@
-import mongoose ,{Types} "mongoose"
+import mongoose  from "mongoose"
 
-const E_commerce = new eCommerceSechma = ({
+const UserSechma  = new mongoose.Sechma = ({
+username: {
+ type : string,
+required: true ,
+unique: true, 
+lowercase: true, 
+},
+email : {
+type : string,
+required: true ,
+unique: true, 
+lowercase: true
+},
+password:{
+type : string,
+required: true ,
+ } ,
 
-},(timestamp: true))
+} , {timestamp: true})
+
+
+export const User =  mongoose.model("User" UserSchma)
