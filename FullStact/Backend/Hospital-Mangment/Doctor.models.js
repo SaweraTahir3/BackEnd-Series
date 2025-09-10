872f2db16b595362399f4b891c49ba88e535f8fa
Sawera Tahir Muhammad <141661220+SaweraTahir3@users.noreply.github.com>
2025-09-10 15:11:38 +0500

    Update Doctor.models.js

--- FullStact/Backend/Hospital-Mangment/Doctor.models.js
+++ FullStact/Backend/Hospital-Mangment/Doctor.models.js
@@ -3,10 +3,32 @@ import mongoose from "mongoose"
 cons doctorSchema = new mongoose.schema({
 
 
+name : {
+type : String,
+required : true, 
+},
 
+salary: {
+type : String,
+required : true, 
+},
+qualification : {
+type : String,
+required : true, 
+},
 
+experienceInYears : {
+type : Number,
+default: 0
+},
 
+workInHospital : [
+{
+type : mongoose.Schema Types.ObjecTId ,
+ref : "Hospital", 
 
+}
+]
 
 
 } , {timestamps : true})
